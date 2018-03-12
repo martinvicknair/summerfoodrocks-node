ad5267af393ec68d9a5da84bc3b44447f6afad1e
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-03-12 13:55:10 -0600

    americorps link

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,10 @@ var path = require("path");
 
 module.exports = function(app) {
 	app.get("/", function(req, res) {
-		// this route should render the Handlebars 'form' template
     res.render("index" );
 	});
 
 	app.get("/americorps", function(req, res) {
-		// this route should render the Handlebars 'form' template
     res.render("ameiricorps" );
 	});
 }
