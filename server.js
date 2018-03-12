c3c8ca9ae11e2f444dc63f05cf758165d603dbcc
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-03-11 19:26:25 -0600

    1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 0.0.0.0:3000;
 
 // Requiring our models directory for syncing
 // var db = require("./models");
