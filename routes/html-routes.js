186dd8a05f966cf4e6cc917bf2c57cd618f0b408
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-03-12 13:52:04 -0600

    americorps page initial add

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,4 +5,9 @@ module.exports = function(app) {
 		// this route should render the Handlebars 'form' template
     res.render("index" );
 	});
+
+	app.get("/americorps", function(req, res) {
+		// this route should render the Handlebars 'form' template
+    res.render("ameiricorps" );
+	});
 }
