2ad949433a7e0d9e45f56dc147b3feb691835954
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-03-11 23:13:55 -0600

    1

--- martinv.js
+++ martinv.js
@@ -31,7 +31,7 @@ app.set("view engine", "handlebars");
 // require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-app.listen(PORT, '0.0.0.0', function() {
+app.listen(PORT, function() {
   console.log("App listening on port:" + PORT);
 });
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,9 +5,4 @@ module.exports = function(app) {
 		// this route should render the Handlebars 'form' template
     res.render("index" );
 	});
-
-	app.get("summerfoodrocks.io/", function(req, res) {
-		// this route should render the Handlebars 'form' template
-		console.log("sfr");
-	});
 }
