163e47c359e5e987dd592eab7748d0533990df6c
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-03-21 12:39:43 -0600

    sequelize datatypes

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,27 @@
+-- * Create the `sfrio_db`.
+-- * Switch to or use the `sfrio_db`.
+-- * Create a `Searches` table with these fields:
+--   * **id**: an auto incrementing int that serves as the primary key.
+--   * **burger_name**: a string.
+--   * **devoured**: a boolean.
+--   * **date**: a TIMESTAMP.
+
+### Schema
 DROP DATABASE IF EXISTS sfrio_db;
 CREATE DATABASE sfrio_db;
 
--- SELECT * FROM ;
+-- -- following not used in a sequelized database
+-- USE sfrio_db;
+--
+-- DROP TABLE Searches;
+-- CREATE TABLE Searches
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	burger_name varchar(255) NOT NULL,
+-- 	devoured BOOLEAN DEFAULT false,
+--   date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+-- 	PRIMARY KEY (id)
+-- );
+--
+-- -- write insert queries to seed the `Searches` table with an entry.
+-- INSERT INTO Searches (burger_name, devoured) VALUES ('Hamburguesa', false);
--- models/search.js
+++ models/search.js
@@ -1,60 +1,36 @@
 module.exports = function(sequelize, DataTypes) {
   var Search = sequelize.define("Search", {
-    dateAdded: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 100]
-      }
-    },
     logText: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 100]
-      }
+      allowNull: false
     },
     numResults: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "Personal"
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     searchTerms: {
-      // type: DataTypes.INTEGER,
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [10],
-        is: ["^[0-9]+$", 'i']
-      }
+      allowNull: false
     },
     searchX: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 100],
-        isEmail: true,
-      }
+      type: DataTypes.DECIMAL,
+      allowNull: false
     },
     searchY: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "Personal"
+      type: DataTypes.DECIMAL,
+      allowNull: false
     },
     userNeighborhood: {
       type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "Personal"
+      allowNull: false
     },
     userX: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "Personal"
+      type: DataTypes.DECIMAL,
+      allowNull: false
     },
     userY: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "Personal"
+      type: DataTypes.DECIMAL,
+      allowNull: false
     }
   });
   // be sure to return the model!
