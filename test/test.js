c9e9af2e1b0a23267457b38fb4a5b605de67f860
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-03-26 13:49:43 -0600

    testing.js

--- test/test.js
+++ test/test.js
@@ -1,15 +1,1 @@
-var expect = require("chai").expect;
-
-var formatPhoneNumber = require("logic.js")
-
-describe("formatPhoneNumber", function() {
-  it("should format a string of numbers into xxx-xxx-xxxx", function() {
-    expect formatPhoneNumber(0005551212)).to.equal(000-555-1212);
-  });
-
-  it("should throw when not passed numbers", function() {
-    expect(function() {
-      formatPhoneNumber("4");
-    }).to.throw(Error);
-  });
-});
+// This is where testing starts
