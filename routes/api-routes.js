1f32851be5dc3d688d81ab99a93c49941c9de55a
Martin Vicknair <martinvicknair@gmail.com>
2019-06-09 15:48:58 -0600

    tweet wording

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -51,11 +51,11 @@ module.exports = function(app) {
       // console.log(newSearch);
       // res.redirect("/api/searches")
 
-      // client.post('statuses/update', {status: '#SFSP #SummerMeals' + '\n' + '\n' + req.body.logText + '\n' + '\n'  + 'Find Free Summer Meals near you: https://SummerFoodRocks.org'}, function(error, tweet, response) {
-      //   if (!error) {
-      //     // console.log(tweet);
-      //   }
-      // });
+      client.post('statuses/update', {status: '#SFSP #SummerMeals' + '\n' + '\n' + req.body.logText + '\n' + '\n'  + 'Find Free Summer Meals near you: https://SummerFoodRocks.org'}, function(error, tweet, response) {
+        if (!error) {
+          // console.log(tweet);
+        }
+      });
 
     });
   });
