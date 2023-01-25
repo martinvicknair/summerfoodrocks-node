39520127ca587c9f584b663ceb1f8a8f0d2d4ae7
Martin <martinvicknair@gmail.com>
2023-01-24 23:58:47 -0600

    port

--- sfrserver.js
+++ sfrserver.js
@@ -8,7 +8,8 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+// var PORT = process.env.PORT || 3000;
+var PORT = 3000;
 
 // Requiring our models directory for syncing
 var db = require("./models");
